5886e2a260bf0c24937b4a88158c0cc9152b2286
nucl3on <59328705+nucl3on@users.noreply.github.com>
2020-10-20 18:24:05 -0400

    removed unnecessary lines

--- simhash-detect.py
+++ simhash-detect.py
@@ -1,20 +1,15 @@
-import re
 import argparse
 from simhash import Simhash
-
 parser = argparse.ArgumentParser(description='Compare Similarity of Two TXT files using Simhash.',usage='%(prog)s file1-location file2-location')
 parser.add_argument('File1Location', metavar="location1", type=str, nargs=1, help='Enter the location of the file1')
 parser.add_argument('File2Location', metavar="location2", type=str, nargs=1, help='Enter the location of the file2')
 args = parser.parse_args()
-# need to add regex to clean user input.
-clean1 = args.File1Location[0]
-clean2 = args.File2Location[0]
 
 try:
-    with open(clean1) as chunk:
-        bytes1 = chunk.read()
-    with open(clean2) as chunk:
-        bytes2 = chunk.read()
+    with open(args.File1Location[0]) as data:
+        bytes1 = data.read()
+    with open(args.File2Location[0]) as data:
+        bytes2 = data.read()
 except:
     print ("\n\nFile Read Error/Not Found!")
     print ("Check your file name/location again \n(or) check for invalid characters/bytes in the file(s).\n")
