00866b8aaa527857867ac03fe5bfc1876a4aa37b
nucl3on <59328705+nucl3on@users.noreply.github.com>
2020-10-20 13:27:40 -0400

    Modified script to compare two files and show distance.

---
+++ compare.py
@@ -0,0 +1,38 @@
+import re
+import argparse
+from simhash import Simhash
+
+parser = argparse.ArgumentParser(description='Compare Similarity of Two Docs using Simhash -nucl3on',usage='%(prog)s file1-location file2-location')
+parser.add_argument('File1Location', metavar="location1", type=str, nargs=1, help='Enter the location of the file1')
+parser.add_argument('File2Location', metavar="location2", type=str, nargs=1, help='Enter the location of the file2')
+args = parser.parse_args()
+# need to add path regex to clean user input.
+clean1 = args.File1Location[0]
+clean2 = args.File2Location[0]
+
+def get_features(s):
+    width = 3
+    s = s.lower()
+    s = re.sub(r'[^\w]+', '', s)
+    return [s[i:i + width] for i in range(max(len(s) - width + 1, 1))]
+
+try:
+    with open(clean1) as chunk:
+        bytes1 = chunk.read()
+    with open(clean2) as chunk:
+        bytes2 = chunk.read()
+except:
+    print ("\nFile Not Found/Access Error!")
+    print ("Check your file name/location again.\n")
+else:
+    hash1 = Simhash(bytes1)
+    hash2 = Simhash(bytes2)
+    """DEBUG
+    print ("\nFile1 Obj:",hash1)
+    print ("File1 Features:",Simhash(get_features(bytes1)).value)
+    print ("File2 Obj:",hash2)
+    print ("File2 Features:",Simhash(get_features(bytes2)).value)
+    """
+    print ("\nFile1 hash.VALUE:",hash1.value)
+    print ("File2 hash.VALUE:",hash2.value)
+    print ("\nDISTANCE:",hash1.distance(hash2),"\n")
